a07b1644351feb3580766a01dca4ba0decfb4b97
Albiglittle <molchanov.art13@gmail.com>
2016-11-19 02:55:37 +0300

    yep

---
+++ offers/admin.py
@@ -0,0 +1,16 @@
+# from django.contrib import admin
+# from offers.models import Offer
+# # Register your models here.
+#
+#
+# # class ArticleInline(admin.StackedInline):
+# #     model = Comments
+# #     extra = 2
+#
+#
+# class ArticleAdmin(admin.ModelAdmin):
+#     fields = ['article_title', 'article_text', 'article_date']
+#     inlines = [ArticleInline]
+#     list_filter = ['article_date']
+#
+# admin.site.register(Article, ArticleAdmin)
---
+++ offers/models.py
@@ -0,0 +1,22 @@
+import datetime
+
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Offer(models.Model):
+    class Meta:
+        db_table = "offer"
+    offer_title = models.CharField(max_length=200)
+    offer_address = models.TextField()
+    article_date = models.DateTimeField()
+
+#
+# class Comments(models.Model):
+#     class Meta:
+#         db_table = "comments"
+#
+#     comments_date = models.DateTimeField(auto_now_add=True)
+#     comments_text = models.TextField(verbose_name='Текст комментария')
+#     comments_article = models.ForeignKey(Article)
+#     comments_from = models.ForeignKey(User)
